94c93ff52494240194e44bed435efea6c88d174a
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2023-01-08 13:57:47 -0600

    week 1 push
    week 1 push

---
+++ week-1/profile-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
